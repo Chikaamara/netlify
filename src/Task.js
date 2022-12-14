1fcf82d6effcc2356430942002e2324f9fc8a395
Chikaamara <chintanakasih@upi.edu>
2022-12-14 16:30:44 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Chintanakasih Amarasully" // TODO: replace this
+const studentId = "FE3051416" // TODO: replace this
+
+export { url, studentName, studentId }
